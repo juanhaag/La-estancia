0c6c34b96caa361f25b7cfe9731553c05d5579ce
Haag Juan Martin <apetroxtrm@gmail.com>
2021-10-11 21:40:05 -0300

    Se agrego funciones para ingresar elementos del carrito al wp

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,26 +2,48 @@ const cardContainer = document.getElementById("list-card")
 
 document.addEventListener('DOMContentLoaded', traerDatos);
 //Variable global que acumula los productos
-let productoGlobal=[];
+let productoGlobal = [];
 
-const carrito= [];
+const carrito = [];
+
+let acumularTitulosCarrito = '';
 
 cardContainer.addEventListener('click', e => {
     ingresarCarrito(e);
 });
 
-function ingresarCarrito(e){
+function ingresarCarrito(e) {
+    let targetEvento = e.target;
+    if (targetEvento.classList.contains('btn-secondary')) {
+        console.log(productoGlobal[targetEvento.dataset.id])
+        carrito.push(productoGlobal[targetEvento.dataset.id]);
+    }
+    if (targetEvento.classList.contains('btn-primary')) {
+        comprar();
+    }
+}
+
+function comprar() {
+    let targetEvento = document.getElementById('comprar-btn');
+    if (targetEvento.classList.contains('comprar-btn')) {
+        recorrerCarrito();
+        generarWp(acumularTitulosCarrito);
+    }
+}
+//Agrega en un string los titulos de los productos para despues pasarlo a generarWp()
+function recorrerCarrito() {
+    carrito.forEach(producto => {
+        acumularTitulosCarrito = acumularTitulosCarrito  + '  ' + producto.titulo + ',';
+    });
+}
+//recive los titulos y abre la ventana de wp
+function generarWp(acumularTitulosCarrito) {
     let setBtnComprarWp = document.getElementById("enviar-carrito");
-    if(e.target.classList.contains('btn-secondary')){
-        console.log(productoGlobal[e.target.dataset.id])
-        carrito.push(productoGlobal[e.target.dataset.id]);
-        //Genenerar btn comprar por wp
-        setBtnComprarWp.innerHTML= `<a href="https://api.whatsapp.com/send?phone=5492262301420&text=Hola, Quisiera comprar estos productos!
-        ${carrito[0].titulo}
+    setBtnComprarWp.innerHTML = `<a target="blank" href="https://api.whatsapp.com/send?phone=5492262301420&text=Hola, Quisiera comprar estos productos!
+        ${acumularTitulosCarrito}
         ">
         <button class="btn btn-primary" >Enviar carrito por wp
         </button></a>`
-    }
 }
 
 function traerDatos() {
